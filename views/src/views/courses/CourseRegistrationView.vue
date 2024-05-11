30eed2a2f0f4025c1940d2d8570a900829c149cd
Muzoora <melvinmuzoora@gmail.com>
2024-05-11 14:26:58 +0300

    Changes made

---
+++ views/src/views/courses/CourseRegistrationView.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    <h1>This is a CourseRegistration</h1>
+</div>
+    
+</template>
+
+
+<script>
+export default {name: "CourseRegistration"}
+</script>
+
+
+<style >
+
+</style>
